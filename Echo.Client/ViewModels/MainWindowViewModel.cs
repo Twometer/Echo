48e4910c73d250621087ac651b0a78be6f48c853
Twometer <twometer@outlook.de>
2020-03-22 21:58:42 +0100

    Setup project

---
+++ Echo.Client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Echo.Client.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
