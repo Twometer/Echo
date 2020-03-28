0da62f4c042bb2ee3995256b5620a09fe36c1b32
Twometer <twometer@outlook.de>
2020-03-28 19:02:35 +0100

    Don't send empty voice samples

--- Echo.Client.Wpf/Voice/VoiceManager.cs
+++ Echo.Client.Wpf/Voice/VoiceManager.cs
@@ -6,6 +6,8 @@ using Echo.Client.Network;
 using Echo.Network.Packets.Udp;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +21,16 @@ namespace Echo.Client.Wpf.Voice
 
         private WriteableBufferingSource source;
 
+        private volatile bool running = true;
+
+        public void EndStreaming()
+        {
+            running = false;
+        }
+
         public void BeginStreaming()
         {
-            waveIn = new WaveIn(new WaveFormat(44100, 16, 1)); // 
+            waveIn = new WaveIn(new WaveFormat(44100, 16, 1)); 
             waveIn.Device = WaveInDevice.DefaultDevice;
             waveIn.Latency = 25;
             waveIn.Initialize();
@@ -29,7 +38,7 @@ namespace Echo.Client.Wpf.Voice
             
             waveIn.Start();
 
-            source = new WriteableBufferingSource(waveIn.WaveFormat); // new WaveFormat(44100, 16, 1)
+            source = new WriteableBufferingSource(waveIn.WaveFormat);
 
             soundOut = new WasapiOut();
             soundOut.Latency = 25;
@@ -41,7 +50,7 @@ namespace Echo.Client.Wpf.Voice
 
         private async void Receiver()
         {
-            while (true)
+            while (running)
             {
                 var packet = await EchoClient.Instance.VoiceClient.PacketStream.ReadPacket();
                 if (packet is U02VoiceData voice)
@@ -53,11 +62,40 @@ namespace Echo.Client.Wpf.Voice
             }
         }
 
+        private int emptySamples = 0;
+
         private void WaveIn_DataAvailable(object sender, DataAvailableEventArgs e)
         {
             var data = new byte[e.ByteCount];
             Buffer.BlockCopy(e.Data, 0, data, 0, e.ByteCount);
+
+            var loudness = GetLoudness(data);
+            if (loudness < 80)
+            {
+                emptySamples++;
+            }
+            else
+            {
+                emptySamples = 0;
+            }
+
+            if (emptySamples > 8)
+                return;
+
             _ = EchoClient.Instance.VoiceClient.SendVoiceData(data);
+        }
+
+        private float GetLoudness(byte[] pcm)
+        {
+            float avg = 0;
+
+            var ms = new BinaryReader(new MemoryStream(pcm));
+            for (int i = 0; i < pcm.Length / 2; i++)
+                avg += Math.Abs(ms.ReadInt16());
+
+            avg /= pcm.Length / 2;
+
+            return avg;
         }
     }
 }
